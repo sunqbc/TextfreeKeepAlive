8e275ef55e41dc3793333d1dcdde6081fc259fe6
sunqbc <66704756+sunqbc@users.noreply.github.com>
2020-07-06 12:07:43 -0700

    Update _textfree.py

--- scripts/_textfree.py
+++ scripts/_textfree.py
@@ -7,6 +7,6 @@ username = os.environ["TEXTNOW_USERNAME"]
 password = os.environ["TEXTNOW_PASSWORD"]
 numbers = os.environ["TEXTNOW_NUMBER"]
 msg = os.environ["TEXTNOW_MSG"]
-text = textfree_sms.Textnow(username, password, numbers, msg)
+text = textfree_sms.textfree(username, password, numbers, msg)
 
 text.send_text()
