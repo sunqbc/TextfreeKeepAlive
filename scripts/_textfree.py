051bf299c33e5eef8547cbf63dd0b9f8ab40c69a
sunqbc <66704756+sunqbc@users.noreply.github.com>
2020-07-06 12:05:12 -0700

    Create _textfree.py

---
+++ scripts/_textfree.py
@@ -0,0 +1,12 @@
+#coding: utf-8
+
+from textfree import textfree_sms
+import os
+
+username = os.environ["TEXTNOW_USERNAME"]
+password = os.environ["TEXTNOW_PASSWORD"]
+numbers = os.environ["TEXTNOW_NUMBER"]
+msg = os.environ["TEXTNOW_MSG"]
+text = textfree_sms.Textnow(username, password, numbers, msg)
+
+text.send_text()
