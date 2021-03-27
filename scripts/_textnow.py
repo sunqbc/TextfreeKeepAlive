495db7c4e4f066d36d27d9cccc4359e18530d1bd
sunqbc <66704756+sunqbc@users.noreply.github.com>
2021-03-27 00:25:59 -0700

    Update _textnow.py

--- scripts/_textnow.py
+++ scripts/_textnow.py
@@ -7,6 +7,6 @@ username = os.environ["TEXTNOW_USERNAME"]
 password = os.environ["TEXTNOW_PASSWORD"]
 numbers = os.environ["TEXTNOW_NUMBER"]
 msg = os.environ["TEXTNOW_MSG"]
-text = textnow_sms.Textnow(username, password, numbers, msg)
+#text = textnow_sms.Textnow(username, password, numbers, msg)
 
-text.send_text()
+#text.send_text()
